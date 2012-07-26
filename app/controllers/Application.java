9cef8130dc72231d5a44d6e75dc41e9f17d4c455
Peter Dietz <pdietz84@gmail.com>
2012-07-25 23:11:12 -0400

    Connect to REST, and display top level community
    Its currently only showing the first top-level community, but a good start

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,90 @@
 package controllers;
 
+import models.Community;
+import org.codehaus.jackson.map.annotate.JacksonStdImpl;
 import play.*;
+import play.api.templates.Html;
 import play.mvc.*;
+import play.libs.Json;
 
 import views.html.*;
+import views.html.index;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.codehaus.jackson.JsonNode;
+import views.html.main;
+
+import javax.swing.text.html.HTML;
+
 
 public class Application extends Controller {
+    private static String baseRestUrl = "http://localhost:8280/rest/";
+
   
   public static Result index() {
-    return ok(index.render("Your new application is ready."));
+      StringBuilder sb = new StringBuilder();
+
+      try {
+          URL url = new URL(baseRestUrl + "communities.json?topLevelOnly=true");
+
+          HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+          conn.setRequestMethod("GET");
+          conn.setRequestProperty("Accept", "application/json");
+
+          if (conn.getResponseCode() != 200) {
+              throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+          }
+
+          BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+          String output;
+          while ((output = br.readLine()) != null) {
+              sb.append(output);
+
+          }
+
+          JsonNode jn = Json.parse(sb.toString());
+
+          List<Community> communities = new ArrayList<Community>();
+
+          List<JsonNode> commNodes = jn.findValues("communities_collection");
+          for(JsonNode commNode : commNodes) {
+              List<String> ids = commNode.findValuesAsText("id");
+              List<String> names = commNode.findValuesAsText("name");
+
+
+              int firstID = Integer.parseInt(ids.get(0));
+              String firstName = names.get(0);
+
+              Community community = new Community();
+              community.id = (long) firstID;
+              community.name = firstName;
+              communities.add(community);
+          }
+
+
+          conn.disconnect();
+
+          return ok(views.html.index.render(communities, "Top Level Communities", sb.toString()));
+
+      } catch (MalformedURLException e) {
+
+          e.printStackTrace();
+
+      } catch (IOException e) {
+
+          e.printStackTrace();
+
+      }
+
+      return ok("Error");
   }
-  
 }
---
+++ app/models/Community.java
@@ -0,0 +1,27 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: peterdietz
+ * Date: 7/20/12
+ * Time: 10:59 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Community {
+    public Long id;
+    public String name;
+
+    public static List<Community> all() {
+        return new ArrayList<Community>();
+    }
+
+    public static void create(Community community) {
+    }
+
+
+    public static void delete(Long id) {
+    }
+}
