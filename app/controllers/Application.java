9102f20d5b817aa6f430f7cee4b1665dd51d9976
Peter Dietz <peter@longsight.com>
2014-10-06 23:25:00 -0400

    Move community index/show to own controller

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -42,100 +42,11 @@ public class Application extends Controller {
     }
 
 
-  public static Result index() {
-      StringBuilder contentString = new StringBuilder();
-      HttpURLConnection conn = null;
-      BufferedReader reader = null;
 
-      try {
-          conn = connectToURL("communities");
 
-          reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-
-          String output;
-          while ((output = reader.readLine()) != null) {
-              contentString.append(output);
-          }
-
-          JsonNode jsonNode = Json.parse(contentString.toString());
-
-          List<Community> communities = new ArrayList<Community>();
-
-          if(jsonNode.size()>0) {
-              for(JsonNode comm : jsonNode) {
-                  Community community = Community.parseCommunityFromJSON(comm);
-                  communities.add(community);
-              }
-          }
-
-          String endpoint = conn.getURL().toString();
-          conn.disconnect();
-
-          return ok(views.html.index.render(communities, "Top Level Communities", contentString.toString(), endpoint));
-
-      } catch (MalformedURLException e) {
-          return badRequest("MalformedURLException: " + e.getMessage());
-      } catch (IOException e) {
-          return internalServerError("IOException :" + e.getMessage());
-      } finally {
-
-          if (reader != null) {
-              try {
-                  reader.close();
-              } catch (IOException e) {
-              }
-          }
-
-          if (conn != null) {
-              conn.disconnect();
-          }
-      }
-  }
-
-  public static Result showCommunity(Long id) {
-      StringBuilder contentString = new StringBuilder();
-      HttpURLConnection conn = null;
-      BufferedReader reader = null;
-
-      try {
-          conn = connectToURL("communities/" + id.toString() + "?expand=all");
-
-          reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-
-          String output;
-          while ((output = reader.readLine()) != null) {
-              contentString.append(output);
-          }
-
-          JsonNode comm = Json.parse(contentString.toString());
-          Community community = new Community();
-
-          if (comm.size() > 0) {
-              community = Community.parseCommunityFromJSON(comm);
-          }
-
-          String endpoint = conn.getURL().toString();
-
-          return ok(views.html.community.detail.render(community, "Single Community", contentString.toString(), endpoint));
-          
-      } catch (MalformedURLException e) {
-          return badRequest(e.getMessage());
-      } catch (IOException e) {
-          return internalServerError(e.getMessage());
-      } finally {
-
-          if (reader != null) {
-              try {
-                  reader.close();
-              } catch (IOException e) {
-              }
-          }
-
-          if (conn != null) {
-              conn.disconnect();
-          }
-      }
-  }
+    public static Result index() {
+        return redirect(controllers.routes.Communities.index());
+    }
 
     public static Result showCollection(Long id) {
         StringBuilder contentString = new StringBuilder();
